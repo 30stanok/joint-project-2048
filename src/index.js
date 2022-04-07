512d94170de15833fe7c05d3cb7c8548860fb885
poiq <bloodbornps@mail.ru>
2022-04-07 14:54:37 +0700

    animated fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-const block = document.getElementById('gamewindow1');
+const block2048 = document.getElementById('gamewindow1');
+const Ticblock = document.getElementById(`gamewindow2`);
 
 const styleSwap = [
     `x2`,
     `x4`,
     `x8`,
-    `x16`,    
+    `x16`,
     `x32`,
     `x64`,
     `x128`,
@@ -13,21 +14,49 @@ const styleSwap = [
     `x1024`,
     `x2048`,
     `x4096`,
-    `x9192`,
 ];
 
+//      обработка первого блока с игрой 2048
+
 let styleCount = 0;
-function firstblock() {
-    setInterval(() => {     //интервал засунуть в фор и поменять на таймаут 
-        for(let i = 0; i < styleSwap.length; i++) {
-            block.innerHTML = styleSwap[i].slice(1);
-            block.classList.add(styleSwap[++i]);
-            if (i > 0) block.classList.remove(styleSwap[i]);
-            if (styleCount == 12) {
-                styleCount = 0;
-            }
-        }
-    }, 2000);
+function animation2048block() {
+    setInterval(() => {
+        colorswitch();
+        styleCount = styleCount + 1;
+        if (styleCount >= 12) styleCount = 0;
+    }, 750);
 }
 
-firstblock();
+const colorswitch = () => {
+    block2048.classList.add(styleSwap[styleCount]);
+    block2048.innerHTML = styleSwap[styleCount].slice(1);
+    if (styleCount == 0) block2048.classList.remove(styleSwap[styleSwap.length - 1]); 
+    else block2048.classList.remove(styleSwap[styleCount - 1]);
+}
+
+animation2048block();
+
+//      обработка первого блока с игрой TicTack
+
+let TicTackAnimationCount = 0;
+Ticblock.style.backgroundColor = `white`;
+Ticblock.style.color = 'red';
+function xORo() {
+    let conxORo = 0
+    setInterval(() => {
+        if (conxORo == 0) {
+            Ticblock.innerHTML = `X`;
+            conxORo = 1;
+        } else {
+            Ticblock.innerHTML = `O`;
+            conxORo = 0;
+        }
+    }, 750);
+}
+
+xORo();
+
+
+//      обработка канваса со змейкой
+
+const canvastb = document.getElementById('snake'); 
